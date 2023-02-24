549bd877b139368c6eb9760529b321826a42b342
EmanueleDev0 <112651240+EmanueleDev0@users.noreply.github.com>
2023-02-24 16:50:52 +0100

    we

--- test_docker.php
+++ test_docker.php
@@ -1,13 +1,11 @@
 <?php
 
-/*$connect = mysqli_connect(
+$connect = mysqli_connect(
     'db', 
     'php_docker', 
     'password', 
     'php_docker' 
-);*/
-
-include "db.php";
+);
 
 $table_name = "Test";
 
