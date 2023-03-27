cf06486ba5f1eeefd46b77b8ab926c006b78c6cc
EmanueleDev0 <112651240+EmanueleDev0@users.noreply.github.com>
2023-03-27 16:51:55 +0200

    Eliminato index per pulizia mentale

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-	$servername = "localhost";
-	$username = "php_docker";
-	$password = "password";
-	$dbname = "php_docker";
 	
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
 ?>
