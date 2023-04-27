b08576e5ba3c688579c7da13ce8df44e45b32207
EmanueleDev0 <emanuelerusso.dev@gmail.com>
2023-04-27 12:15:21 +0200

    Fix db_conn per linux Aggiornamento database

--- db/Database.sql
+++ db/Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Creato il: Apr 18, 2023 alle 17:51
+-- Creato il: Apr 27, 2023 alle 10:12
 -- Versione del server: 8.0.32
 -- Versione PHP: 8.1.17
 
@@ -20,36 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `php_docker`
 --
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `Azienda`
---
-
-CREATE TABLE `Azienda` (
-  `ID_Azienda` int NOT NULL,
-  `Nome_Azienda` varchar(128) NOT NULL,
-  `Indirizzo` varchar(128) NOT NULL,
-  `Tipologia` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `Offerte di Lavoro`
---
-
-CREATE TABLE `Offerte di Lavoro` (
-  `ID_Azienda` int NOT NULL,
-  `Nome_Azienda` varchar(128) NOT NULL,
-  `Ruolo` varchar(128) NOT NULL,
-  `Periodo` varchar(128) NOT NULL,
-  `Stipendio` varchar(128) NOT NULL,
-  `Zona` varchar(128) NOT NULL,
-  `Ore` int NOT NULL,
-  `Posti_Disponibili` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -74,25 +44,16 @@ CREATE TABLE `Utente` (
 --
 
 INSERT INTO `Utente` (`Email`, `Nome`, `Cognome`, `Ruolo`, `Matricola`, `id_azienda`, `id_universita`, `password_hash`, `ID`) VALUES
+('cicciomontecucco@gmail.com', 'Ciccio', 'Montecucco', 'Studente', 123456, NULL, NULL, '$2y$10$evvBUf.iKMGjjxDPAV0UhOOPe0CtL.ze.xHlDxFYAsNfdJePajnr2', 4),
 ('cicciorusso@gmail.com', 'Ciccio', 'Russo', 'Studente', 444444, NULL, NULL, '$2y$10$MahDjXnfH24HOdkLbUPr1.7QnfCbne8.Yr4Ec7lNfG9wUeWyGjWGG', 3),
 ('davidemento02@gmail.com', 'Davide', 'Mento', 'Studente', 527917, NULL, NULL, '$2y$10$m4hMGRQkiUJv8TmEViQcQui.LRe9iZO2UasGSdUVDPZlmcOKCkHhC', 1),
-('emanuelerusso02@gmail.com', 'Emanuele', 'Russo', 'Studente', 539827, NULL, NULL, '$2y$10$PesSa9fGbdwzMxor2mmID.TE8Egf1.Z.iJUGLdE9rtgbCIqVjAUiq', 2);
+('emanuelerusso02@gmail.com', 'Emanuele', 'Russo', 'Studente', 539827, NULL, NULL, '$2y$10$PesSa9fGbdwzMxor2mmID.TE8Egf1.Z.iJUGLdE9rtgbCIqVjAUiq', 2),
+('mvillari@gmai.com', 'Mimmo', 'Villari', 'Studente', 323232, NULL, NULL, '$2y$10$/HlQEWMxVRj2YMVjtdsNj.K5573Jvq5tmrodtVZkPT.ZhVKHX8t5i', 6),
+('sbasalari@gmail.com', 'Steven', 'Basalari', 'Esercente', NULL, 111111, NULL, '$2y$10$eySTuSk1Qr3uzUKRd4Ua6uTU0CTm.kXDgVE0O2DQxRd2kRRf7hrZq', 5);
 
 --
 -- Indici per le tabelle scaricate
 --
-
---
--- Indici per le tabelle `Azienda`
---
-ALTER TABLE `Azienda`
-  ADD PRIMARY KEY (`ID_Azienda`);
-
---
--- Indici per le tabelle `Offerte di Lavoro`
---
-ALTER TABLE `Offerte di Lavoro`
-  ADD PRIMARY KEY (`ID_Azienda`);
 
 --
 -- Indici per le tabelle `Utente`
@@ -109,7 +70,7 @@ ALTER TABLE `Utente`
 -- AUTO_INCREMENT per la tabella `Utente`
 --
 ALTER TABLE `Utente`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
