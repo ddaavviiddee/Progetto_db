b815fc88cbbe4c79f907bb76c88187f3d9a49d8d
EmanueleDev0 <emanuelerusso.dev@gmail.com>
2023-06-12 08:57:05 +0200

    terminato

--- firma-contratto.php
+++ firma-contratto.php
@@ -18,6 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST'){
     $id_contratto = $_POST['id_contratto'];
     $id_domanda = $_POST['id_domanda'];
     $ore = $_POST["ore"];
+    $ore .= ' Settimanali';
     $periodo = $_POST['periodo'];
     $stipendio = $_POST['stipendio'];
     $nome_azienda = $_POST['nome_azienda'];
